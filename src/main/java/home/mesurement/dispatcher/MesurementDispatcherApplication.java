d4ece0b3ea9cc636b1fc635c535c2641b2064902
Roman Josipović <rjosipovic@gmail.com>
2019-03-27 19:53:10 +0100

    Initial commit

---
+++ src/main/java/home/mesurement/dispatcher/MesurementDispatcherApplication.java
@@ -0,0 +1,13 @@
+package home.mesurement.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesurementDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesurementDispatcherApplication.class, args);
+	}
+
+}
